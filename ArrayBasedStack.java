204edb6bcb51a266d1c54de353a64d27d1ad1064
Victoria Nguyen <nguyenv25@egr-maclab-03.local>
2018-02-15 15:13:42 -0500

    New ArrayBasedStack

--- ArrayBasedStack.java
+++ ArrayBasedStack.java
@@ -1,10 +1,12 @@
+import java.util.Arrays;
+import java.util.Stack;
 public class ArrayBasedStack <T> implements StackInterface <T>{
       
    private T[] data;
    //(T[]) new Object[length]; 
    private int topOfStack;
    private int INITIAL_CAPACITY = 5; 
-   private boolean initialized = true;   
+   private boolean initialized;   
    
    public ArrayBasedStack(int length){
       data = (T[]) new Object[length];
@@ -12,13 +14,27 @@ public class ArrayBasedStack <T> implements StackInterface <T>{
       if(topOfStack < 0){
          throw new IllegalArgumentException("Please input a value above 0");
       }
+      initialized = true;
+   }
+   public ArrayBasedStack(){
+    INITIAL_CAPACITY = 5;
+   }
+   
+   private int DoubleArray(){
+      T[] newArray = Arrays.copyOf(data,(data.length * 2));
+      return 1;
    }
    
    /** Adds a new entry to the top of this stack.
        @param newEntry  An object to be added to the stack. */
    public void push(T newEntry){
-      
+      Stack<T> myStack = new Stack<T>(); 
+           
    }
+   public void expandArray(){
+   
+   }
+  
   
    /** Removes and returns this stack's top entry.
        @return  The object at the top of the stack. 
@@ -37,7 +53,12 @@ public class ArrayBasedStack <T> implements StackInterface <T>{
    /** Detects whether this stack is empty.
        @return  True if the stack is empty. */
    public boolean isEmpty(){
-      return true;
+      if(topOfStack < 0){
+         return false;
+      }
+      else{
+         return true;
+      }
    }
   
    /** Removes all entries from this stack. */
